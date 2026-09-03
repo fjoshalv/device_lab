61361c22bc566220b4cfd875a5209d2c122dbf13
Joshua Alvarado <fjoshalv@gmail.com>
2026-09-02 22:40:16 -0600

    [chore] M4 handle plugin and platform exceptions

--- lib/src/presentation/device_info_screen.dart
+++ lib/src/presentation/device_info_screen.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+
 import 'package:device_lab/src/data/native_apis/device_info_api.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart'
+    show MissingPluginException, PlatformException;
 
 class DeviceInfoScreen extends StatefulWidget {
   const DeviceInfoScreen({super.key});
@@ -24,21 +28,48 @@ class _DeviceInfoScreenState extends State<DeviceInfoScreen> {
           mainAxisAlignment: .center,
           children: <Widget>[
             FilledButton(
-              onPressed: () async {
-                final deviceInfoApi = DeviceInfoApi();
-                final batteryLevelResult = await deviceInfoApi.getBatteryInfo();
-                final deviceInfoResult = await deviceInfoApi.getDeviceInfo();
-
-                setState(() {
-                  nativeMessage = '$batteryLevelResult%\n$deviceInfoResult';
-                });
-              },
+              onPressed: _getDeviceInfo,
               child: const Text('Get device info'),
             ),
             const SizedBox(
               height: 30,
             ),
             Text('Battery Level: $nativeMessage'),
+          ],
+        ),
+      ),
+    );
+  }
+
+  /// Can throw a [PlatformException] or [MissingPluginException] if the native
+  /// code fails to get the device info or the method does not exist or isn't
+  /// handled.
+  Future<void> _getDeviceInfo() async {
+    final deviceInfoApi = DeviceInfoApi();
+    try {
+      final batteryLevelResult = await deviceInfoApi.getBatteryInfo();
+      final deviceInfoResult = await deviceInfoApi.getDeviceInfo();
+
+      setState(() {
+        nativeMessage = '$batteryLevelResult%\n$deviceInfoResult';
+      });
+    } catch (e) {
+      _showErrorDialog();
+    }
+  }
+
+  void _showErrorDialog() {
+    unawaited(
+      showDialog<void>(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: const Text('Error'),
+          content: const Text('Failed to get device info.'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text('OK'),
+            ),
           ],
         ),
       ),
